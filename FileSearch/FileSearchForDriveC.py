c1a249871973d3532869f7c3ac2fcb55204be42b
enfor9 <nvf69r1p3qs@yahoo.co.jp>
2021-11-17 19:04:18 +0900

    20211117 modify 検索対象ディレクトリを選べるように機能改善

--- FileSearch/FileSearchForDriveC.py
+++ FileSearch/FileSearchForDriveC.py
@@ -1,10 +1,29 @@
 import glob
 #Cドライブ配下のファイルを検索する処理
 
-#検索対象のファイル名を入力させる。
-print("Please input search word.")
-target = 'C:\**\\' + input()
+#検索対象ディレクトリ
+searchDir = ''
+#検索ワード
+searchWord = ''
+#検索条件
+searchCondition = ''
+
+#検索対象ディレクトリ
+
+while searchDir == '':
+  print("Please enter a directory to search. > ")
+  searchDir = input()
+
+#検索対象ディレクトリの末尾が「\」でない場合はtargetの末尾に「\」を追加する
+if searchDir[-1] != '\\':
+  searchDir = searchDir + '\\'
+
+#検索ワードを入力させる
+while searchWord == '':
+  print("Please enter search word. > ")
+  searchWord = input()
+  searchCondition = searchDir + '**\\' + searchWord
 
 # Cドライブ配下の検索処理を実施する。再帰検索有効。
-for name in glob.glob(target,recursive=True):
+for name in glob.glob(searchCondition,recursive=True):
   print(name)
