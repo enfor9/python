85e689b4a9e79317bf9a67694f0722c22bd06d0f
enfor9 <nvf69r1p3qs@yahoo.co.jp>
2021-11-17 19:07:56 +0900

    20211117 modify コメント改善

--- FileSearch/FileSearchForDriveC.py
+++ FileSearch/FileSearchForDriveC.py
@@ -1,5 +1,5 @@
 import glob
-#Cドライブ配下のファイルを検索する処理
+#ファイルを検索する処理
 
 #検索対象ディレクトリ
 searchDir = ''
@@ -8,17 +8,16 @@ searchWord = ''
 #検索条件
 searchCondition = ''
 
-#検索対象ディレクトリ
-
+#検索対象ディレクトリを入力させる。
 while searchDir == '':
   print("Please enter a directory to search. > ")
   searchDir = input()
 
-#検索対象ディレクトリの末尾が「\」でない場合はtargetの末尾に「\」を追加する
+#検索対象ディレクトリの末尾が「\」でない場合はtargetの末尾に「\」を追加する。
 if searchDir[-1] != '\\':
   searchDir = searchDir + '\\'
 
-#検索ワードを入力させる
+#検索ワードを入力させる。
 while searchWord == '':
   print("Please enter search word. > ")
   searchWord = input()
